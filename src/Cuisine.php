8f2306fc9f0c4b5d4b09a9bce2479d5329a5d918
aundra miller and matt knutson <mknut001@gmail.com>
2016-02-24 08:25:01 -0800

    add initial project files and folders

---
+++ src/Cuisine.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Cuisine {
+
+        private $type;
+        private $id;
+
+        function __construct($type, $id = null)
+        {
+            $this->type = $type;
+            $this->id = $id;
+        }
+
+
+    }
+
+
+
+
+?>
